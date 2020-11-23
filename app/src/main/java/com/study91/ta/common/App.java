3bb9a020a9d00c35051a6117a93f5b93418206cf
study91 <1964073@qq.com>
2020-11-23 09:11:33 +0800

    更新全局com.study91.ta.common.App类，获取全局Context。

--- app/src/main/java/com/study91/ta/common/App.java
+++ app/src/main/java/com/study91/ta/common/App.java
@@ -1,4 +1,27 @@
 package com.study91.ta.common;
 
-public class App {
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * App全局类
+ * App启动时需要加载此类：
+ * 在AndroidManifest.xml文件中添加：android:name=".common.App"
+ */
+public class App extends Application {
+    private static Context context; //应用程序上下文
+
+    /**
+     * 获取全局应用程序上下文
+     * @return 应用程序上下文
+     */
+    public static Context getContext() {
+        return App.context;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        App.context = getApplicationContext(); //应用程序上下文
+    }
 }
